cb4680be664f904e47b1da813ef9cd995e04a5b5
Gielpy <leverette.welsh@gmail.com>
2014-05-28 14:02:15 -0700

    Added code to pull pitch_type data

--- players.rb
+++ players.rb
@@ -2,10 +2,11 @@ require 'csv'
 require 'open-uri'
 require 'nokogiri'
 
+#updated 5-28-14 by GLP
 #Create a temporary csv file to save the updated information
 total_pitch_csv = CSV.open('../total_pitch.csv', 'w')
 #Create the CSV headers
-total_pitch_csv << ['Player', 'Player ID', 'TJ Surgery Date', 'Team', 'Majors', 'Pitches', 'FB%', 'FBv', 'SL%', 'SLv', 'CT%', 'CTv', 'CB%', 'CBv', 'CH%', 'CHv', 'SF%', 'SFv', 'KN%', 'KNv', 'XX%']
+total_pitch_csv << ['Player', 'Player ID', 'TJ Surgery Date', 'Team', 'Majors', '# of pitches', 'FB%', 'FBv', 'SL%', 'SLv', 'CT%', 'CTv', 'CB%', 'CBv', 'CH%', 'CHv', 'SF%', 'SFv', 'KN%', 'KNv', 'XX%']
 #get the number of pitches per player
 players = CSV.foreach('../updated_tjanalysis.csv', headers:true) do |row|
 	#grab player id from the csv, form the url, get the page, pass it to Nokogiri
@@ -16,9 +17,10 @@ players = CSV.foreach('../updated_tjanalysis.csv', headers:true) do |row|
 
 		#Convert date. The given date from the CSV is 5/20/2014. It needs to be
 		#2014-05-19 for the url. 
-		raw_end_date = r-ow['TJ Surgery Date']
+		raw_end_date = row['TJ Surgery Date']
 		end_date = raw_end_date.split('/').reverse.join("-")
-		url = "http://www.fangraphs.com/statsd.aspx?playerid=#{id}&position=P&type=4&gds=&gde=#{end_date}&season=all"
+		start_date = 1900-01-01
+		url = "http://www.fangraphs.com/statsd.aspx?playerid=#{id}&position=P&type=4&gds=#{start_date}&gde=#{end_date}&season=all"
 		puts url
 		html = open(url)
 		pitcher_page = Nokogiri::HTML(html)
@@ -28,22 +30,31 @@ players = CSV.foreach('../updated_tjanalysis.csv', headers:true) do |row|
 		#or the pitching total.
 		total_pitches = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[15]').text
 		puts row['Player'] + " " + total_pitches
+		
+		url = "http://www.fangraphs.com/statsd.aspx?playerid=#{id}&position=P&type=6&gds=&gde=#{end_date}&season=all"
+		puts url
+		html = open(url)
+		pitcher_page = Nokogiri::HTML(html)
+		
+		pitch_type_1 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[5]').text
+		pitch_type_2 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[6]').text
+		pitch_type_3 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[7]').text
+		pitch_type_4 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[8]').text
+		pitch_type_5 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[9]').text
+		pitch_type_6 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[10]').text
+		pitch_type_7 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[11]').text
+		pitch_type_8 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[12]').text
+		pitch_type_9 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[13]').text
+		pitch_type_10 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[14]').text
+		pitch_type_11 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[15]').text
+		pitch_type_12 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[16]').text
+		pitch_type_13 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[17]').text
+		pitch_type_14 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[18]').text
+		pitch_type_15 = pitcher_page.xpath('//table/tbody/tr[@id="DailyStats1_dgSeason1_ctl00__0"]/td[19]').text
 
-		url2= "http://www.fangraphs.com/statsd.aspx?playerid=#{id}&position=P&type=6&gds=&gde#{end_date}=&season=all"
-		puts url2
-		html = open(url2)
-		pitchtype_page = Nokogiri::HTML(html)
-		fbper=[]
-		puts fbper[5]
-		for i in 5..19
-			fbper[i] = pitchtype_page.xpath('//table/tbody/tr
-[@id="DailyStats1_dgSeason1_ct100__0"]/td[i]').text
-		puts row['Player'] + " " + fbper[i]
-		end
 
 		#Write to the new csv file
-		updated_row =  [row['Player'], id, row['TJ Surgery Date'], row['Team'], row['Majors'], total_pitches, fbper[5], fbper[6],fbper[7], fbper[8], fbper[9], fpber[10], fbper[11]
-fbper[12], fbper[13], fbper[14], fbper[15], fbper[16], fbper[17], fbper[18], fbper[19]]
+		updated_row =  [row['Player'], id, row['TJ Surgery Date'], row['Team'], row['Majors'], total_pitches, pitch_type_1, pitch_type_2, pitch_type_3, pitch_type_4, pitch_type_5, pitch_type_6, pitch_type_7, pitch_type_8, pitch_type_9, pitch_type_10, pitch_type_11, pitch_type_12, pitch_type_13, pitch_type_14, pitch_type_15]
 		total_pitch_csv.puts updated_row
 	end
 end
